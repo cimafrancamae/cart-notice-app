76c6b636b0537617a7d3f46edcaef6cbd23f00b6
maeci <cimafrancamae@gmail.com>
2025-02-27 17:48:24 +0800

    feat: add custom discount

--- extensions/local-pickup-discount-function/src/run.ts
+++ extensions/local-pickup-discount-function/src/run.ts
@@ -2,9 +2,6 @@ import type {
   RunInput,
   FunctionRunResult
 } from "../generated/api";
-import {
-  DiscountApplicationStrategy,
-} from "../generated/api";
 
 const EMPTY_DISCOUNT: FunctionRunResult = {
   discounts: [],
@@ -13,8 +10,40 @@ const EMPTY_DISCOUNT: FunctionRunResult = {
 type Configuration = {};
 
 export function run(input: RunInput): FunctionRunResult {
-  const configuration: Configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
-  return EMPTY_DISCOUNT;
-};
+  let selectedPickupInStore = false;
+  const deliveryOptions: {
+    deliveryOption: {
+      handle: string
+    }
+  }[] = [];
+
+  for (const deliveryGroup of input.cart.deliveryGroups) {
+    for (const option of deliveryGroup.deliveryOptions) {
+      if (option.title === "Pickup in store") {
+        selectedPickupInStore = true;
+        deliveryOptions.push({
+          deliveryOption: {
+            handle: option.handle
+          }
+        });
+      }
+    }
+  }
+
+  if (!selectedPickupInStore) {
+    return EMPTY_DISCOUNT;
+  }
+
+  return {
+    discounts: [
+      {
+        targets: deliveryOptions,
+        value: {
+          fixedAmount: {
+            amount: 20
+          }
+        }
+      }
+    ]
+  };
+}
